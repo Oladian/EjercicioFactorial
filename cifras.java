406990d5b3a5934d0cf88425138261d19dcbaf8f
Izan <ios2asir@gmail.com>
2017-11-07 11:44:08 +0100

    Añadido parte del ejercicio 2: cifras

---
+++ cifras.java
@@ -0,0 +1,13 @@
+public class cifras {
+	public static int cuentaCifras(int numero){
+
+		int resto=0;
+		int num=0;
+		int contador=0;
+		int cifra1=10;
+		int cifra2=100;
+		int cifra3=1000;
+
+		return contador;
+	}
+}
